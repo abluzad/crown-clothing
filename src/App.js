ab435172ac4b4244d1a743827fcd67595a94e787
abluzad <73598341+abluzad@users.noreply.github.com>
2023-05-14 18:35:59 +0200

    seperated the navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <h2>You can shop here now</h2>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
