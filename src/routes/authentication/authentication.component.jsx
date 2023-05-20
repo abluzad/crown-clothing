3946465e8e5c45832f6220c806838c89eec17f29
abluzad <73598341+abluzad@users.noreply.github.com>
2023-05-20 17:48:45 +0200

    added the sign-in component and finished the authentication

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,18 +1,13 @@
-import {
-  singInWithGooglePopup,
-  createUserDocument,
-} from "../../utils/firebase/firebase.utils";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
+import "./authentication.style.scss";
+
 const Authentication = () => {
-  const logGoogleUser = async () => {
-    const { user } = await singInWithGooglePopup();
-    createUserDocument(user);
-  };
   return (
-    <div>
-      <h1>Sign In page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    <div className="authentication-container">
+      <SignInForm />
       <SignUpForm />
     </div>
   );
