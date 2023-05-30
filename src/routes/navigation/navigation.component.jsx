dc8ac71c10a9d15a4769b9a0d937663535993346
abluzad <73598341+abluzad@users.noreply.github.com>
2023-05-30 13:37:47 +0200

    created product context and a shopping cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,20 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment, useContext } from "react";
+
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
 import "./navigation.style.scss";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
+
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-
+  const { isCartOpen } = useContext(CartContext);
   // console.log(currentUser);
   return (
     <Fragment>
@@ -28,7 +35,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
